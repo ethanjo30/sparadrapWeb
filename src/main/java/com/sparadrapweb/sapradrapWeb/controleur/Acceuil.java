764d89905548bd7e55941d18fe7a78589249fcb4
ethanjo30 <ethan.jorand24@gmail.com>
2024-01-15 17:00:19 +0100

    modif du 15/01/24

--- src/main/java/com/sparadrapweb/sapradrapWeb/controleur/Acceuil.java
+++ src/main/java/com/sparadrapweb/sapradrapWeb/controleur/Acceuil.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class Acceuil {
-    @GetMapping("/")
+    @GetMapping("/Acceuil")
     public String home(Model model) {
 		return "Page/acceuil";
 	}
