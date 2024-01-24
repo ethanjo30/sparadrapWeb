0693dbfc4ba2772ac9df8f99246f6108adde8619
ethanjo30 <ethan.jorand24@gmail.com>
2024-01-24 16:38:57 +0100

    modif du 24/01/24

--- src/main/resources/static/js/SansOrdo.js
+++ src/main/resources/static/js/SansOrdo.js
@@ -1,6 +1,26 @@
 let patient = document.getElementById("patient")
-console.log(patient)
 
 let medicament = document.getElementById("medicament")
-console.log(medicament)
 
+let listmedicament = document.querySelectorAll(".listmedicament")
+
+let table = document.querySelector("#tableAchat tbody")
+
+if (medicament != null){
+   /* ajouter du nom du medicament selectionner */
+
+    medicament.addEventListener("input", () => {
+
+        if(medicament.value !== "") {
+           tbody = document.querySelector(".listmedicament")
+           tbody.innerHTML = "<tr th:each='medicament: ${medicaments}'>";
+           tbody.innerHTML = "<td><span th:text='${medicament.nom_Med}'></span></td>";
+           tbody.innerHTML = "<td><span th:text='${medicament.cate_Med}'></span></td>";
+
+            let nouvelleLigne = table.insertRow();
+            let cellnom = nouvelleLigne.insertCell(0);
+
+            cellnom.textContent = medicament.value;
+        }
+    });
+}
