186e186f09d104629c931e3b8d9077087c2ca217
ethanjo30 <ethan.jorand24@gmail.com>
2024-01-26 11:56:47 +0100

    modif du 26/01/24

--- src/main/resources/static/js/SansOrdo.js
+++ src/main/resources/static/js/SansOrdo.js
@@ -15,7 +15,7 @@ medicament.addEventListener("change", () => {
         for (const row of data){
             if (medicament.value===row.innerText){
                 categorie = row.getAttribute("cate_Med");
-                prix = row.getAttribute("prix_Med");
+                prix = parseFloat(row.getAttribute("prix_Med"));
             }
         }
         
@@ -25,14 +25,25 @@ medicament.addEventListener("change", () => {
             <td class="nomMed">${nom}</td>
             <td class="catMed">${categorie}</td>
             <td class="prixMed">${prix} €</td>
-            <td class="quantite"><input class="form-control quantite" value="1"></td>
-            <td class="total"></td>`;
+            <td class="quantite"><input value="1"></td>
+            <td class="total">${prix} €</td>`;
 
             listmedicament.forEach(element => {
                 element.appendChild(newRow.cloneNode(true));
             });
-        
-        let prixMedicament = document.querySelector(".prixMed")
-        console.log(prixMedicament.value)
+
+            let colQuantite = newRow.querySelector(".quantite input");
+            let colTotal = newRow.querySelector(".total");
+
+            console.log(colQuantite.value)
+            console.log(colTotal.textContent)
+            console.log(prix)
+
+            colQuantite.addEventListener("change", () => {
+                let totalLigne = prix * colQuantite.value;
+                colTotal.textContent = totalLigne.toFixed(2) + " €";
+                console.log()
+            });
+
     }
 }); 
